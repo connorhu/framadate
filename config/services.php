13a89765ff8a55bd9b61135ae5c866194fb02f79
Connor <connor@connor.hu>
2024-03-09 15:49:56 +0100

    feature: added database service (#21)

--- config/services.php
+++ config/services.php
@@ -1,11 +1,28 @@
 <?php
 
 use Connor\DoReMi\Application;
+use Framadate\FramaDB;
 use Psr\Container\ContainerInterface;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Definition;
+
+function configDatabase(ContainerBuilder $builder, Application $application): void
+{
+    $definition = new Definition(FramaDB::class);
+    $definition->setArguments([
+        $application->getConfiguration()->getDatabaseDsn(),
+        $application->getConfiguration()->getDatabaseUser(),
+        $application->getConfiguration()->getDatabasePassword(),
+    ]);
+    $definition->setPublic(true);
+
+    $builder->setDefinition(FramaDB::class, $definition);
+}
 
 return function (Application $application): ContainerInterface {
     $builder = new ContainerBuilder();
+
+    configDatabase($builder, $application);
 
     $builder->compile();
     return $builder;
