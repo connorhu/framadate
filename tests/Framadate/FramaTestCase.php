a1a6784a5c837b5515c5be134bb6aa1c1b2cffcf
Connor <connor@connor.hu>
2024-03-09 08:15:00 +0100

    feat: new directory structure (#9)
    * reorganized
    
    * moved to config dir
    
    * removed lock

---
+++ tests/Framadate/FramaTestCase.php
@@ -0,0 +1,24 @@
+<?php
+namespace tests\Framadate;
+
+use PHPUnit\Framework\TestCase;
+
+abstract class FramaTestCase extends TestCase {
+    protected function getTestResourcePath(string $resourcepath): string
+    {
+        return __DIR__ . '/../resources/' . $resourcepath;
+    }
+
+    protected function readTestResource(string $resourcepath) {
+        return file_get_contents($this->getTestResourcePath($resourcepath));
+    }
+
+    protected function invoke(&$object, $methodName) {
+        $reflectionClass = new \ReflectionClass($object);
+        $reflectionMethod = $reflectionClass->getMethod($methodName);
+        $reflectionMethod->setAccessible(true);
+
+        $params = array_slice(func_get_args(), 2); // get all the parameters after $methodName
+        return $reflectionMethod->invokeArgs($object, $params);
+    }
+}
---
+++ tests/Framadate/Services/MailServiceUnitTest.php
@@ -0,0 +1,35 @@
+<?php
+namespace tests\Framadate\Services;
+
+use Framadate\Services\MailService;
+use tests\Framadate\FramaTestCase;
+
+class MailServiceUnitTest extends FramaTestCase {
+    public const MSG_KEY = '666';
+
+    public function test_should_send_a_2nd_mail_after_a_good_interval(): void
+    {
+        // Given
+        $mailService = new MailService(true);
+        $_SESSION[MailService::MAILSERVICE_KEY] = [self::MSG_KEY => time() - 1000];
+
+        // When
+        $canSendMsg = $mailService->canSendMsg(self::MSG_KEY);
+
+        // Then
+        $this->assertTrue($canSendMsg);
+    }
+
+    public function test_should_not_send_2_mails_in_a_short_interval(): void
+    {
+        // Given
+        $mailService = new MailService(true);
+        $_SESSION[MailService::MAILSERVICE_KEY] = [self::MSG_KEY => time()];
+
+        // When
+        $canSendMsg = $mailService->canSendMsg(self::MSG_KEY);
+
+        // Then
+        $this->assertFalse($canSendMsg);
+    }
+}
